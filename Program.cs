b45d5dc632fde4cb89e705aef0ab8840bf87cd60
Robert Crawford <robbie.crawford999@gmail.com>
2024-07-30 17:19:43 +0100

    creating repo

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//In Terminal type:
+// dotnet new console
+//^ this creates the Program.cs
+// dotnet build
+//^ this compiles the application before running
+// dotnet run
+//^ automatically builds then runs the code
+
+Console.Clear(); // clears console before the rest of the code runs
+Console.WriteLine("Cheese Sandwich Recipe\n----------------------"); // \n moves next text to a new line
+
+//Color: american spelling :(
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("yellow");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
